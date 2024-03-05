0928dea62b1aa519799454b5deed6684549d5d08
nakra <nakra@DHARMIK>
2024-03-05 10:22:28 -0500

    Add project files.

---
+++ TestClass/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using TriangleSolver;
+
+namespace TestClass
+{
+    public class Class1
+    {
+        [Test]
+        public void ValidEquilateralTriangleTest()
+        {
+            var side1 = 10;
+            var side2 = 10;
+            var side3 = 10;
+
+            var expected = "Equilateral triangle";
+
+            // Create an instance of your triangle class with sides for a valid equilateral triangle
+            var actual = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+    }
+}
